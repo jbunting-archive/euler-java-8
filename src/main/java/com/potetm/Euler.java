3a36a119e00ee34a3dbc54b23414ede603561a32
Tim Pote <tim.pote@cicayda.com>
2014-04-29 07:30:35 -0500

    Initial commit.

---
+++ src/main/java/com/potetm/Euler.java
@@ -0,0 +1,43 @@
+package com.potetm;
+
+public class Euler {
+    public static long problem1() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem2() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem3() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem4() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem5() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem6() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem7() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem8() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem9() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem10() {
+        return Long.MIN_VALUE;
+    }
+}
