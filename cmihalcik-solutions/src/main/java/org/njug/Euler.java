48144ef2e8e2c8cb24a44cbceb70845e1ca91ef3
Chris Mihalcik <chris.mihalcik@gmail.com>
2014-05-05 21:01:35 -0500

    Created personal project and solved problem 1

---
+++ cmihalcik-solutions/src/main/java/org/njug/Euler.java
@@ -0,0 +1,50 @@
+package org.njug;
+
+import java.util.stream.IntStream;
+
+public class Euler {
+
+    public static long problem1() {
+        return IntStream.range(1,1000)
+                .filter(i -> i % 5 == 0 || i % 3 == 0)
+                .sum();
+    }
+
+
+    public static long problem2() {
+        
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem3() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem4() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem5() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem6() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem7() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem8() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem9() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem10() {
+        return Long.MIN_VALUE;
+    }
+}
---
+++ cmihalcik-solutions/src/test/java/org/njug/EulerTest.java
@@ -0,0 +1,60 @@
+package org.njug;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Solutions from: https://code.google.com/p/projectEuler-solutions/wiki/ProjectEulerSolutions
+ */
+public class EulerTest {
+    @Test
+    public void problem1() {
+        assertEquals(233168, Euler.problem1());
+    }
+
+    @Test
+    public void problem2() {
+        assertEquals(4613732, Euler.problem2());
+    }
+
+    @Test
+    public void problem3() {
+        assertEquals(6857, Euler.problem3());
+    }
+
+    @Test
+    public void problem4() {
+        assertEquals(906609, Euler.problem4());
+    }
+
+    @Test
+    public void problem5() {
+        assertEquals(232792560, Euler.problem5());
+    }
+
+    @Test
+    public void problem6() {
+        assertEquals(25164150, Euler.problem6());
+    }
+
+    @Test
+    public void problem7() {
+        assertEquals(104743, Euler.problem7());
+    }
+
+    @Test
+    public void problem8() {
+        assertEquals(40824, Euler.problem8());
+    }
+
+    @Test
+    public void problem9() {
+        assertEquals(31875000, Euler.problem9());
+    }
+
+    @Test
+    public void problem10() {
+        assertEquals(142913828922l, Euler.problem10());
+    }
+}
