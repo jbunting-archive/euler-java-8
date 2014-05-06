20b7e21731f7d997033fddd15c091776416883b9
Jared Bunting <jared.bunting@peachjean.com>
2014-05-06 18:19:34 -0500

    Creating my solution project.

---
+++ jbunting-solutions/src/main/java/org/njug/Euler.java
@@ -0,0 +1,45 @@
+package org.njug;
+
+public class Euler {
+
+    public static long problem1() {
+        return Long.MIN_VALUE;
+    }
+
+
+    public static long problem2() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem3() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem4() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem5() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem6() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem7() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem8() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem9() {
+        return Long.MIN_VALUE;
+    }
+
+    public static long problem10() {
+        return Long.MIN_VALUE;
+    }
+}
